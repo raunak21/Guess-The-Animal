c59308e0305d9effe3b5b67e350a1b43467086fa
Raunak <raunak21@gmail.com>
2017-10-11 22:13:18 +0530

    First commit

---
+++ GuessTheAnimal/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GuessTheAnimal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lbOptions = new System.Windows.Forms.ListBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(853, 64);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "G U E S S   T H E   A N I M A L";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbOptions
+            // 
+            this.lbOptions.FormattingEnabled = true;
+            this.lbOptions.ItemHeight = 18;
+            this.lbOptions.Location = new System.Drawing.Point(12, 111);
+            this.lbOptions.Name = "lbOptions";
+            this.lbOptions.Size = new System.Drawing.Size(829, 220);
+            this.lbOptions.TabIndex = 1;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(12, 80);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(829, 17);
+            this.lblInstructions.TabIndex = 2;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(729, 337);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(112, 37);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "&Start";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(12, 337);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(112, 37);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "&Restart Game";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnNext;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 384);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lbOptions);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess The Animal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ListBox lbOptions;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ GuessTheAnimal/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheAnimal
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variables
+
+        DataSet dsXML;
+
+        bool gameStarted = false;
+        int currentParameterIndex = 0;
+        string currentParameter = string.Empty, filterQuery = string.Empty;
+
+        #endregion
+
+        #region Init & Events
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+            #region Find & display all animals
+
+            List<string> lstAnimals = new List<string>();
+            lstAnimals.Add("Choose an animal in your head");
+            foreach (DataRow drAnimal in dsXML.Tables["Animal"].Rows)
+            {
+                lstAnimals.Add(drAnimal["Name"].ToString());
+            }
+            lbOptions.DataSource = lstAnimals;
+            lbOptions.Enabled = false;
+
+            #endregion
+
+            lblInstructions.Text = "From the list below, choose an animal in your head and click the Start button";
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (gameStarted == false)
+            {
+                btnNext.Text = "&Next";
+                gameStarted = true;
+                lbOptions.Enabled = true;
+                GuessAnimalOrDisplayOptions();
+            }
+            else
+            {
+                if (filterQuery != string.Empty)
+                    filterQuery += " AND ";
+
+                filterQuery += currentParameter += " = '" + lbOptions.SelectedItem.ToString() + "'";
+
+                GuessAnimalOrDisplayOptions();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Loads data from XML file into dsXML object
+        /// </summary>
+        private void LoadData()
+        {
+            string xmlPath = Path.Combine((new FileInfo(Application.ExecutablePath).DirectoryName), "Data.xml");
+            dsXML = new DataSet();
+            dsXML.ReadXml(xmlPath);
+        }
+
+        /// <summary>
+        /// This method tries to guess the animal or displays next question to the user
+        /// </summary>
+        private void GuessAnimalOrDisplayOptions()
+        {
+            //Filter down the dataview on the basis of selected option
+            DataView dvAnimals = new DataView(dsXML.Tables["Animal"]);
+            dvAnimals.RowFilter = filterQuery;
+
+            if (dvAnimals.Count == 1)
+            {
+                //We have narrowed down to a single animal. Display the name of the animal to the user.
+
+                //Disable listbox & next button. Display restart game button.
+                lbOptions.Enabled = false;
+                btnNext.Enabled = false;
+                btnRestart.Visible = true;
+
+                //Display chosen animal
+                lblTitle.ForeColor = Color.DarkGreen;
+                lblTitle.Text = "You had chosen " + dvAnimals[0]["Name"] + "!";
+                MessageBox.Show("You had chosen " + dvAnimals[0]["Name"] + "!", "We have guessed your animal!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //Set focus on the restart button
+                btnRestart.Focus();
+            }
+            else
+            {
+                //Couldn't filter down to a single animal. Display the next parameter/question with distinct options.
+
+                //Get the next parameter and update instructions
+                currentParameter = dsXML.Tables["QuestionSequence"].Rows[currentParameterIndex]["ParameterName"].ToString();
+                lblInstructions.Text = dsXML.Tables["QuestionSequence"].Rows[currentParameterIndex]["Question"].ToString();
+                currentParameterIndex++;
+
+                //Find distinct options
+                List<string> lstOptions = new List<string>();
+                foreach (DataRowView drvAnimal in dvAnimals)
+                {
+                    string option = drvAnimal[currentParameter].ToString();
+                    if (lstOptions.Contains(option) == false)
+                        lstOptions.Add(option);
+                }
+
+                if (lstOptions.Count == 1)
+                {
+                    //There's only once distinct choice for the current parameter. Call the function again to move to the next parameter.
+                    GuessAnimalOrDisplayOptions();
+                }
+                else
+                {
+                    //Display the options to the user and set focus on the listbox
+                    lbOptions.DataSource = lstOptions;
+                    lbOptions.Focus();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
